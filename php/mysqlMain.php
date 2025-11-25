c9dc0da1329f56b26640bc96a75cecc0445d6f6c
Hasullon <168424144+Hasullon@users.noreply.github.com>
2025-11-25 18:35:59 +0100

    Update mysqlMain.php

--- php/mysqlMain.php
+++ php/mysqlMain.php
@@ -1,6 +1,11 @@
 <?php
 $name = "";
 
+$db_host = getenv('DB_HOST') ?: 'localhost';
+$db_user = getenv('DB_USER') ?: 'root';
+$db_pass = getenv('DB_PASSWORD');
+$db_name = getenv('DB_NAME') ?: 'Pokewebapp';
+
 $link = mysqli_connect("localhost","root","","Pokewebapp");
 
 if ($link == false) {
